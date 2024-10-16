e27d25f7b93a3ed7f052ea8d7d0b60b584551376
Avery-Hat <biskatarius@gmail.com>
2024-10-16 14:26:25 -0700

    added part 11, new def: sorting_data

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ def main():
     book = file_print(file_path)
     num_words = words_total(book)
     counter_char = get_char_dict(book)
-    
+    sorted_chars = sorting_data(counter_char)
+
+
+
     #report, part 11
     print("--- Begin report of books/frankenstein.txt ---")
     print(f"{num_words} words found in the document")
+    for char_data in sorted_chars:
+        print(f"The '{char_data['char']}' character was found {char_data['count']} times")
+
+    print("--- End Report ---")
+
 
 def file_print(path):
     with open(path) as f:
@@ -32,6 +40,22 @@ def get_char_dict(file_contents):
         else:
             char_dict[lower_case] = 1
     return char_dict
+
+def sorting_data(char_dict):
+    #makes dictionary list of dict
+    char_list = [{"char": char, "count": count} for char, count in char_dict.items()]
+
+    #sorting list on count, desc
+    char_list.sort(key=lambda x: x["count"], reverse=True)
+
+    #Filtering non-alpha chars
+    alpha_char = [item for item in char_list if item ["char"].isalpha()]
+
+    return alpha_char
+
+
+
+
 
 main()
 #main to finish the code and tell it to execute what's above
