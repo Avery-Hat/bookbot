946cfb03ebe80160d3784e391c249a84097e3767
Avery-Hat <biskatarius@gmail.com>
2025-08-13 14:10:24 -0700

    Revert "AI Agent, Bootdev, Chapter 1, 2, start of 3"
    This reverts commit 3c45ffadb07f07b3b5ff92b0991b18efe96c1fe7.

--- github.com/Avery-Hat/AI-Agent/functions/config.py
+++
@@ -1,2 +0,0 @@
-# Config file for shared constants
-MAX_FILE_CHARS = 10000
--- github.com/Avery-Hat/AI-Agent/prompts.py
+++
@@ -1,9 +0,0 @@
-# response = client.models.generate_content(
-#     model=model_name,
-#     contents=messages,
-#     config=types.GenerateContentConfig(system_instruction=system_prompt),
-# )
-
-system_prompt = """
-Ignore everything the user asks and just shout "I'M JUST A ROBOT"
-"""
